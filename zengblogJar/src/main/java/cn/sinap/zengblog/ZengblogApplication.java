e629ec924691d80e9aaf896639e6102fd52390f5
Vealroer <852028438@qq.com>
2021-07-02 13:20:44 +0800

    加入注释

--- zengblogJar/src/main/java/cn/sinap/zengblog/ZengblogApplication.java
+++ zengblogJar/src/main/java/cn/sinap/zengblog/ZengblogApplication.java
@@ -3,6 +3,7 @@ package cn.sinap.zengblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//启动类
 @SpringBootApplication
 public class ZengblogApplication {
 
